7e8903b052759254e35d7d38d7821af257caba8a
LaEscobarGit <nataliaescobarhdez@gmail.com>
2026-09-14 22:30:54 -0700

    TAREA - [PROF] - US1 - ACT4 Codificación de la capa de vista (Java) del alta de profesores.
    Avance

---
+++ vista/src/main/java/mx/desarrollo/ui/ProfesorBean.java
@@ -0,0 +1,47 @@
+package mx.desarrollo.ui;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import mx.desarrollo.entity.Profesor;
+import mx.desarrollo.helper.ProfesorHelper;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+@Named("profesorUI")
+@SessionScoped
+public class ProfesorBean implements Serializable{
+    private ProfesorHelper profesorHelper;
+    private Profesor profesor;
+
+    public ProfesorBean() {
+        profesorHelper = new ProfesorHelper();
+    }
+    @PostConstruct
+    public void init(){
+        profesor = new Profesor();
+    }
+
+    public void registrar(){
+        boolean registrado = profesorHelper.registrar(profesor.getNombre(), profesor.getApellidoPat(), profesor.getApellidoMat(), profesor.getRfc());
+        if(registrado){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Profesor registrado:", "El profesor se agrego al catalogo."));
+        }else{
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error de registro:", "Hubo un error al hacer el registro en la BD."));
+        }
+    }
+    public void buscar(int id){
+        Profesor p = profesorHelper.buscar(id);
+    }
+    public void consultar(){
+        List<Profesor> listaProfes = profesorHelper.consultar();
+    }
+    
+    /* getters y setters*/
+    public Profesor getProfesor() {
+        return profesor;
+    }
+}
