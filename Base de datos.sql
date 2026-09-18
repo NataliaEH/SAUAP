cbf54dadf051ffcd274f321c83cfded325a088a2
NataliaEH <natalia.escobar@uabc.edu.mx>
2026-09-18 10:27:18 -0700

    Agregar script de BD actualizado
    Se actualizó el tipo de dato de horaFinal y horaInicio de la tabla Horario a TIME

---
+++ Base de datos.sql
@@ -0,0 +1,168 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sauap
+-- ------------------------------------------------------
+-- Server version	9.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE SCHEMA IF NOT EXISTS `SAUAP` DEFAULT CHARACTER SET utf8 ;
+USE `SAUAP` ;
+
+--
+-- Table structure for table `asignacion`
+--
+
+DROP TABLE IF EXISTS `SAUAP`.`asignacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SAUAP`.`asignacion` (
+  `id_asignacion` int NOT NULL AUTO_INCREMENT,
+  `id_profesor` int NOT NULL,
+  `grupo` int NOT NULL,
+  `id_ua` int NOT NULL,
+  PRIMARY KEY (`id_asignacion`),
+  KEY `id_profesor` (`id_profesor`),
+  KEY `id_ua` (`id_ua`),
+  CONSTRAINT `asignacion_ibfk_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id_profesor`),
+  CONSTRAINT `asignacion_ibfk_2` FOREIGN KEY (`id_ua`) REFERENCES `unidad_de_aprendizaje` (`id_ua`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asignacion`
+--
+
+LOCK TABLES `SAUAP`.`asignacion` WRITE;
+/*!40000 ALTER TABLE `SAUAP`.`asignacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SAUAP`.`asignacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `horario`
+--
+
+DROP TABLE IF EXISTS `SAUAP`.`horario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SAUAP`.`horario` (
+  `id_horario` int NOT NULL AUTO_INCREMENT,
+  `dia` varchar(50) DEFAULT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL,
+  `tipo_de_clase` varchar(50) NOT NULL,
+  `id_asignacion` int NOT NULL,
+  PRIMARY KEY (`id_horario`),
+  KEY `id_asignacion` (`id_asignacion`),
+  CONSTRAINT `horario_ibfk_1` FOREIGN KEY (`id_asignacion`) REFERENCES `asignacion` (`id_asignacion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `horario`
+--
+
+LOCK TABLES `SAUAP`.`horario` WRITE;
+/*!40000 ALTER TABLE `SAUAP`.`horario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SAUAP`.`horario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesor`
+--
+
+DROP TABLE IF EXISTS `SAUAP`.`profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SAUAP`.`profesor` (
+  `id_profesor` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido_pat` varchar(50) NOT NULL,
+  `apellido_mat` varchar(50) NOT NULL,
+  `rfc` varchar(13) NOT NULL,
+  PRIMARY KEY (`id_profesor`),
+  UNIQUE KEY `rfc` (`rfc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesor`
+--
+
+LOCK TABLES `SAUAP`.`profesor` WRITE;
+/*!40000 ALTER TABLE `SAUAP`.`profesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SAUAP`.`profesor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unidad_de_aprendizaje`
+--
+
+DROP TABLE IF EXISTS `SAUAP`.`unidad_de_aprendizaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SAUAP`.`unidad_de_aprendizaje` (
+  `id_ua` int NOT NULL AUTO_INCREMENT,
+  `nombre_ua` varchar(50) NOT NULL,
+  `horas_clase` int DEFAULT NULL,
+  `horas_taller` int DEFAULT NULL,
+  `horas_lab` int DEFAULT NULL,
+  PRIMARY KEY (`id_ua`),
+  CONSTRAINT `unidad_de_aprendizaje_chk_1` CHECK ((`horas_clase` between 0 and 4)),
+  CONSTRAINT `unidad_de_aprendizaje_chk_2` CHECK ((`horas_taller` between 0 and 4)),
+  CONSTRAINT `unidad_de_aprendizaje_chk_3` CHECK ((`horas_lab` between 0 and 4))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `unidad_de_aprendizaje`
+--
+
+LOCK TABLES `SAUAP`.`unidad_de_aprendizaje` WRITE;
+/*!40000 ALTER TABLE `SAUAP`.`unidad_de_aprendizaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SAUAP`.`unidad_de_aprendizaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `SAUAP`.`usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SAUAP`.`usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(50) DEFAULT NULL,
+  `contrasena` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `SAUAP`.`usuario` WRITE;
+/*!40000 ALTER TABLE `SAUAP`.`usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SAUAP`.`usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-14 10:13:19
